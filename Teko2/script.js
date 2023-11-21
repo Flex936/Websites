14d92055694f672da43266cfca41a9526efc71e6
Tancsik Alex <tancsik.alex@gmail.com>
2023-11-21 11:32:01 +0100

    uploading websites

---
+++ Teko2/script.js
@@ -0,0 +1,27 @@
+function Home() {
+    document.getElementById('Home').style.display="block"
+    document.getElementById('Events').style.display="none"
+    document.getElementById('Courses').style.display="none"
+    document.getElementById('Activities').style.display="none"
+}
+
+function Events() {
+    document.getElementById('Home').style.display="none"
+    document.getElementById('Events').style.display="block"
+    document.getElementById('Courses').style.display="none"
+    document.getElementById('Activities').style.display="none"
+}
+
+function Courses() {
+    document.getElementById('Home').style.display="none"
+    document.getElementById('Events').style.display="none"
+    document.getElementById('Courses').style.display="block"
+    document.getElementById('Activities').style.display="none"
+}
+
+function Activities() {
+    document.getElementById('Home').style.display="none"
+    document.getElementById('Events').style.display="none"
+    document.getElementById('Courses').style.display="none"
+    document.getElementById('Activities').style.display="block"
+}
